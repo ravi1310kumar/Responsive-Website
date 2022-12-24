bdbac61863b1cd5343b89e23f91aa36bd84cccba
ravi1310kumar <117887808+ravi1310kumar@users.noreply.github.com>
2022-12-24 19:54:34 +0530

    Add files via upload

---
+++ website/formhandler.php
@@ -0,0 +1,29 @@
+<?php
+$name= $_POST['name'];
+$visitor_email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+
+$email_from = 'info@easy.gamil.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+             "User Email: $visitor_email.\n".
+             "User Subject: $subject.\n".
+            "User Message: $message.\n";
+                
+
+$to 'ravik@gmail.com';
+
+$headers = "Form: $$email_form \r\n";
+
+$headers = "Reply-To: $$visiter_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers)
+
+header("Location: contact.html");
+
+
+
+?>
